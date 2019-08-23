314e0562da5e0a9e15d277a7b7d6a9db54cd89ba
tranek <1534735+tranek@users.noreply.github.com>
2019-08-23 17:46:03 -0400

    Rewrote Sprint and AimDownSights to be predicted better
    Now using CMC's saved moves.
    
    https://wiki.unrealengine.com/Authoritative_Networked_Character_Movement
    http://www.sabredartstudios.com/

--- Source/GASDocumentation/Private/Characters/GDCharacterMovementComponent.cpp
+++ Source/GASDocumentation/Private/Characters/GDCharacterMovementComponent.cpp
@@ -6,6 +6,12 @@
 #include "GameplayTagContainer.h"
 #include "GDCharacterBase.h"
 
+UGDCharacterMovementComponent::UGDCharacterMovementComponent()
+{
+	SprintSpeedMultiplier = 1.4f;
+	ADSSpeedMultiplier = 0.6f;
+}
+
 float UGDCharacterMovementComponent::GetMaxSpeed() const
 {
 	AGDCharacterBase* Owner = Cast<AGDCharacterBase>(GetOwner());
@@ -25,19 +31,137 @@ float UGDCharacterMovementComponent::GetMaxSpeed() const
 		return 0.0f;
 	}
 
-	if (Owner->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.AimDownSights"))) &&
-		!Owner->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.AimDownSights.Removal"))))
+	if (RequestToStartSprinting)
 	{
-		float ADSSpeed = Owner->GetMoveSpeedBaseValue() * 0.6f;
-		return ADSSpeed < Owner->GetMoveSpeed() ? ADSSpeed : Owner->GetMoveSpeed();
+		return Owner->GetMoveSpeed() * SprintSpeedMultiplier;
 	}
 
-	if (Owner->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Sprint"))) &&
-		!Owner->GetAbilitySystemComponent()->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Sprint.Removal"))))
+	if (RequestToStartADS)
 	{
-		float SprintSpeed = Owner->GetMoveSpeedBaseValue() * 1.4f;
-		return SprintSpeed > Owner->GetMoveSpeed() ? SprintSpeed : Owner->GetMoveSpeed();
+		return Owner->GetMoveSpeed() * ADSSpeedMultiplier;
 	}
 
 	return Owner->GetMoveSpeed();
+}
+
+void UGDCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+
+	//The Flags parameter contains the compressed input flags that are stored in the saved move.
+	//UpdateFromCompressed flags simply copies the flags from the saved move into the movement component.
+	//It basically just resets the movement component to the state when the move was made so it can simulate from there.
+	RequestToStartSprinting = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+
+	RequestToStartADS = (Flags & FSavedMove_Character::FLAG_Custom_1) != 0;
+}
+
+FNetworkPredictionData_Client * UGDCharacterMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != NULL);
+	check(PawnOwner->Role < ROLE_Authority);
+
+	if (!ClientPredictionData)
+	{
+		UGDCharacterMovementComponent* MutableThis = const_cast<UGDCharacterMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FGDNetworkPredictionData_Client(*this);
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+	}
+
+	return ClientPredictionData;
+}
+
+void UGDCharacterMovementComponent::StartSprinting()
+{
+	RequestToStartSprinting = true;
+}
+
+void UGDCharacterMovementComponent::StopSprinting()
+{
+	RequestToStartSprinting = false;
+}
+
+void UGDCharacterMovementComponent::StartAimDownSights()
+{
+	RequestToStartADS = true;
+}
+
+void UGDCharacterMovementComponent::StopAimDownSights()
+{
+	RequestToStartADS = false;
+}
+
+void UGDCharacterMovementComponent::FGDSavedMove::Clear()
+{
+	Super::Clear();
+
+	SavedRequestToStartSprinting = false;
+	SavedRequestToStartADS = false;
+}
+
+uint8 UGDCharacterMovementComponent::FGDSavedMove::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (SavedRequestToStartSprinting)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	if (SavedRequestToStartADS)
+	{
+		Result |= FLAG_Custom_1;
+	}
+
+	return Result;
+}
+
+bool UGDCharacterMovementComponent::FGDSavedMove::CanCombineWith(const FSavedMovePtr & NewMove, ACharacter * Character, float MaxDelta) const
+{
+	//Set which moves can be combined together. This will depend on the bit flags that are used.
+	if (SavedRequestToStartSprinting != ((FGDSavedMove*)&NewMove)->SavedRequestToStartSprinting)
+	{
+		return false;
+	}
+
+	if (SavedRequestToStartADS != ((FGDSavedMove*)&NewMove)->SavedRequestToStartADS)
+	{
+		return false;
+	}
+
+	return Super::CanCombineWith(NewMove, Character, MaxDelta);
+}
+
+void UGDCharacterMovementComponent::FGDSavedMove::SetMoveFor(ACharacter * Character, float InDeltaTime, FVector const & NewAccel, FNetworkPredictionData_Client_Character & ClientData)
+{
+	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	UGDCharacterMovementComponent* CharacterMovement = Cast<UGDCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+		SavedRequestToStartSprinting = CharacterMovement->RequestToStartSprinting;
+		SavedRequestToStartADS = CharacterMovement->RequestToStartADS;
+	}
+}
+
+void UGDCharacterMovementComponent::FGDSavedMove::PrepMoveFor(ACharacter * Character)
+{
+	Super::PrepMoveFor(Character);
+
+	UGDCharacterMovementComponent* CharacterMovement = Cast<UGDCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+	}
+}
+
+UGDCharacterMovementComponent::FGDNetworkPredictionData_Client::FGDNetworkPredictionData_Client(const UCharacterMovementComponent & ClientMovement)
+	: Super(ClientMovement)
+{
+}
+
+FSavedMovePtr UGDCharacterMovementComponent::FGDNetworkPredictionData_Client::AllocateNewMove()
+{
+	return FSavedMovePtr(new FGDSavedMove());
 }
--- Source/GASDocumentation/Public/Characters/GDCharacterMovementComponent.h
+++ Source/GASDocumentation/Public/Characters/GDCharacterMovementComponent.h
@@ -14,6 +14,70 @@ class GASDOCUMENTATION_API UGDCharacterMovementComponent : public UCharacterMove
 {
 	GENERATED_BODY()
 	
+	class FGDSavedMove : public FSavedMove_Character
+	{
+	public:
+
+		typedef FSavedMove_Character Super;
+
+		///@brief Resets all saved variables.
+		virtual void Clear() override;
+
+		///@brief Store input commands in the compressed flags.
+		virtual uint8 GetCompressedFlags() const override;
+
+		///@brief This is used to check whether or not two moves can be combined into one.
+		///Basically you just check to make sure that the saved variables are the same.
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* Character, float MaxDelta) const override;
+
+		///@brief Sets up the move before sending it to the server. 
+		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData) override;
+		///@brief Sets variables on character movement component before making a predictive correction.
+		virtual void PrepMoveFor(class ACharacter* Character) override;
+
+		// Sprint
+		uint8 SavedRequestToStartSprinting : 1;
+
+		// Aim Down Sights
+		uint8 SavedRequestToStartADS : 1;
+	};
+
+	class FGDNetworkPredictionData_Client : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FGDNetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		///@brief Allocates a new copy of our custom saved move
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
 public:
+	UGDCharacterMovementComponent();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sprint")
+	float SprintSpeedMultiplier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Aim Down Sights")
+	float ADSSpeedMultiplier;
+
+	uint8 RequestToStartSprinting : 1;
+	uint8 RequestToStartADS : 1;
+
 	virtual float GetMaxSpeed() const override;
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+	virtual class FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+	// Sprint
+	UFUNCTION(BlueprintCallable, Category = "Sprint")
+	void StartSprinting();
+	UFUNCTION(BlueprintCallable, Category = "Sprint")
+	void StopSprinting();
+
+	// Aim Down Sights
+	UFUNCTION(BlueprintCallable, Category = "Aim Down Sights")
+	void StartAimDownSights();
+	UFUNCTION(BlueprintCallable, Category = "Aim Down Sights")
+	void StopAimDownSights();
 };
