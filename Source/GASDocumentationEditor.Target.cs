6f76de2e7ada40015ef6b58cd4b6b0304328e46f
tranek <1534735+tranek@users.noreply.github.com>
2023-05-27 19:58:03 -0700

    Updated Targets' include order version

--- Source/GASDocumentationEditor.Target.cs
+++ Source/GASDocumentationEditor.Target.cs
@@ -9,7 +9,7 @@ public class GASDocumentationEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("GASDocumentation");
 	}
 }
